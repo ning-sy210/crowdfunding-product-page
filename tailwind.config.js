2f5643ef3d2ccc567f35b6393e92a861b7700bc5
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-10-19 22:41:49 +0800

    config: add "Commissioner" font + screen breakpoints + colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
+    screens: {
+      mb: "375px",
+      dt: "1440px",
+    },
+    colors: {
+      "primary-1": "hsl(176, 50%, 47%)",
+      "primary-2": "hsl(176, 72%, 28%)",
+      "neutral-1": "hsl(0, 0%, 0%)",
+      "neutral-2": "hsl(0, 0%, 48%)",
+    },
+    fontFamily: {
+      sans: ["Commissioner", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
