8fc483d2a83ac7bca936ab4937bde874f14e9849
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-10-20 21:34:36 +0800

    config: extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,17 @@ export default {
       mb: "375px",
       dt: "1440px",
     },
-    colors: {
-      "primary-1": "hsl(176, 50%, 47%)",
-      "primary-2": "hsl(176, 72%, 28%)",
-      "neutral-1": "hsl(0, 0%, 0%)",
-      "neutral-2": "hsl(0, 0%, 48%)",
-    },
     fontFamily: {
       sans: ["Commissioner", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        "primary-1": "hsl(176, 50%, 47%)",
+        "primary-2": "hsl(176, 72%, 28%)",
+        "neutral-1": "hsl(0, 0%, 0%)",
+        "neutral-2": "hsl(0, 0%, 48%)",
+      },
+    },
   },
   plugins: [],
 };
