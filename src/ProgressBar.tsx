989491298c3024e224f79780ee3daee39864ef34
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-10-22 00:12:53 +0800

    fix: inconsistent progress bar completion width

--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -6,7 +6,8 @@ const ProgressBar = ({ progress }: ProgressBarType) => {
   return (
     <div className="w-full h-3 rounded-full bg-slate-100">
       <div
-        className={`w-[${progress}%] h-[inherit] rounded-[inherit] bg-primary-1`}
+        className="h-[inherit] rounded-[inherit] bg-primary-1"
+        style={{ width: `${progress}%` }}
       ></div>
     </div>
   );
