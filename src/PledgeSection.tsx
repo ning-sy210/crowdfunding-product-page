209744146fc92ca5948a0705b0da474d603005e4
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-10-24 22:38:31 +0800

    refactor: add additional "pledge with no reward" option

--- src/PledgeSection.tsx
+++ src/PledgeSection.tsx
@@ -1,32 +1,12 @@
 import { useState } from "react";
+import { PledgeRewards, pledgeOptions } from "./constants/enums";
 
-type PledgeOptionProps = {
-  reward: string;
+export type PledgeOptionProps = {
+  reward: PledgeRewards;
   minPledgeAmt: number;
   desc: string;
   stock: number;
 };
-
-const pledgeOptions: PledgeOptionProps[] = [
-  {
-    reward: "Bamboo Stand",
-    minPledgeAmt: 25,
-    desc: "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you'll be added to a special Backer member list.",
-    stock: 101,
-  },
-  {
-    reward: "Black Edition Stand",
-    minPledgeAmt: 75,
-    desc: "You get a Black Special Edition computer stand and a personal thank you. You'll be added to our Backer member list. Shipping is included.",
-    stock: 64,
-  },
-  {
-    reward: "Mahogany Special Edition",
-    minPledgeAmt: 200,
-    desc: "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You'll be added to our Backer member list. Shipping is included.",
-    stock: 0,
-  },
-];
 
 const PledgeOption = ({
   reward,
@@ -40,7 +20,7 @@ const PledgeOption = ({
   return (
     <section
       className={`flex flex-col items-start p-[22px] gap-y-5 border border-slate-300 rounded-lg${
-        outOfStock && " opacity-50"
+        outOfStock ? " opacity-50" : ""
       }`}
     >
       <section className="flex flex-col gap-y-1">
@@ -72,15 +52,18 @@ const PledgeOption = ({
 const PledgeSection = () => {
   return (
     <>
-      {pledgeOptions.map((option) => (
-        <PledgeOption
-          key={option.reward}
-          reward={option.reward}
-          minPledgeAmt={option.minPledgeAmt}
-          desc={option.desc}
-          stock={option.stock}
-        />
-      ))}
+      {pledgeOptions.map(
+        (option) =>
+          option.minPledgeAmt > 0 && (
+            <PledgeOption
+              key={option.reward}
+              reward={option.reward}
+              minPledgeAmt={option.minPledgeAmt}
+              desc={option.desc}
+              stock={option.stock}
+            />
+          )
+      )}
     </>
   );
 };
