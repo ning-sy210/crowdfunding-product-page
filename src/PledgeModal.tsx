be7b2ccd45c781d9066e27add1a27a182d2057b3
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-11-01 12:17:49 +0800

    feat: add amount to pledge

--- src/PledgeModal.tsx
+++ src/PledgeModal.tsx
@@ -19,42 +19,67 @@ const PledgeModalOption = ({
 
   return (
     <section
-      className={`flex flex-col gap-y-5 px-6 pt-5 pb-7 rounded-lg border border-slate-300${
+      className={`rounded-lg border border-slate-300${
         outOfStock ? " opacity-50" : ""
       } [&:has(input:checked)]:border-primary-1 [&:has(input:checked)]:border-2`}
       onClick={outOfStock ? () => null : onClick}
     >
-      <div className="flex items-center gap-x-4">
-        <input
-          id={`${reward} input`}
-          type="radio"
-          disabled={outOfStock}
-          checked={checked}
-          onClick={onClick}
-          className="w-6 h-6 bg-primary-1"
-        />
-        <label
-          htmlFor={`${reward} input`}
-          className="flex flex-col h-12 justify-center"
-        >
-          <div className="flex flex-col gap-y-1 justify-between">
-            <span className="text-h5 font-bold">{reward}</span>
+      <section className="flex flex-col gap-y-5 px-6 pt-5 pb-7">
+        <div className="flex items-center gap-x-4">
+          <input
+            id={`${reward} input`}
+            type="radio"
+            disabled={outOfStock}
+            checked={checked}
+            onClick={onClick}
+            className="w-6 h-6 bg-primary-1"
+          />
+          <label
+            htmlFor={`${reward} input`}
+            className="flex flex-col h-12 justify-center"
+          >
+            <div className="flex flex-col gap-y-1 justify-between">
+              <span className="text-h5 font-bold">{reward}</span>
 
-            {isPaidOption && (
-              <span className="text-h5 font-medium text-primary-1">
-                Pledge ${minPledgeAmt.toLocaleString("en-US")} or more
-              </span>
-            )}
+              {/* Min pledge amount label */}
+              {isPaidOption && (
+                <span className="text-h5 font-medium text-primary-1">
+                  Pledge ${minPledgeAmt.toLocaleString("en-US")} or more
+                </span>
+              )}
+            </div>
+          </label>
+        </div>
+        <p className="text-h5 text-neutral-2 leading-6">{desc}</p>
+
+        {/* Stock left label */}
+        {isPaidOption && (
+          <p className="flex items-center gap-x-2">
+            <span className="text-h4 font-bold">{stock}</span>
+            <span className="text-h5 text-neutral-2 leading-6">left</span>
+          </p>
+        )}
+      </section>
+
+      {/* For indicating amount to be pledged by user */}
+      {checked && (
+        <section className="flex flex-col text-center gap-y-5 pt-6 pb-6 px-6 border-t border-slate-300">
+          <p className="text-h5 text-neutral-2 leading-6">Enter your pledge</p>
+          <div className="relative flex justify-between">
+            <span className="absolute top-[13px] left-6 text-h5 font-bold text-stone-400">
+              $
+            </span>
+            <input
+              type="number"
+              className="w-[43%] border border-slate-300 rounded-full pl-10 text-h5 font-bold"
+              min={minPledgeAmt}
+              defaultValue={minPledgeAmt}
+            ></input>
+            <button className="bg-primary-1 text-white rounded-full w-[50%] py-[13px] text-h5 font-bold">
+              Continue
+            </button>
           </div>
-        </label>
-      </div>
-      <p className="text-h5 text-neutral-2 leading-6">{desc}</p>
-
-      {isPaidOption && (
-        <p className="flex items-center gap-x-2">
-          <span className="text-h4 font-bold">{stock}</span>
-          <span className="text-h5 text-neutral-2 leading-6">left</span>
-        </p>
+        </section>
       )}
     </section>
   );
