a729ce5b607e90d1a17ac8c415b12b133185d59e
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-11-09 21:53:14 +0800

    chore: change from `section` to `div`

--- src/components/mainSection/projectPledgeProgress/ProgressTracker.tsx
+++ src/components/mainSection/projectPledgeProgress/ProgressTracker.tsx
@@ -5,10 +5,10 @@ export type ProgressTrackerProps = {
 
 const ProgressTracker = ({ header, subText }: ProgressTrackerProps) => {
   return (
-    <section>
+    <div>
       <p className="text-h1 font-bold">{header}</p>
       <p className="leading-6 text-h5 text-neutral-2">{subText}</p>
-    </section>
+    </div>
   );
 };
 
