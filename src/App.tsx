e00cc9fb967a12923fc4eeb09b2e932f42821684
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-11-17 21:24:39 +0800

    style: make gap between `<header>` and `<main>` responsive

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
         title="desktop hero image"
       />
 
-      <div className="absolute top-0 left-0 right-0 px-6 pb-20 flex flex-col items-center gap-y-[164px]">
+      <div className="absolute top-0 left-0 right-0 px-6 pb-20 flex flex-col items-center gap-y-[164px] tablet:gap-y-[190px]">
         <AppHeader />
         <MainSection />
       </div>
