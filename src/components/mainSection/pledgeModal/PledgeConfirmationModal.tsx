96b8e7c3dd382691dbb927319df87669f7301120
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-11-22 23:53:34 +0800

    feat: make `<PledgeConfirmationModal />` responsive

--- src/components/mainSection/pledgeModal/PledgeConfirmationModal.tsx
+++ src/components/mainSection/pledgeModal/PledgeConfirmationModal.tsx
@@ -11,18 +11,27 @@ const PledgeConfirmationModal = ({
   closeModal,
 }: PledgeConfirmationProps) => {
   return (
-    <div className="fixed top-0 left-0 right-0 w-full py-36 px-6 z-[1]">
-      <section className="flex flex-col text-center items-center gap-y-8 px-4 pt-8 pb-10 bg-white rounded-lg z-[2]">
+    <div className="fixed top-0 left-0 right-0 w-full py-36 px-6 z-[1] tablet:py-[5.75rem]">
+      <section
+        className="flex flex-col text-center items-center gap-y-8 px-4 pt-8 pb-10 bg-white rounded-lg z-[2] max-w-[33.75rem]
+      tablet:mx-auto tablet:px-12 tablet:py-12"
+      >
         {isLoading ? (
           <div className="h-32 flex items-center">
             <HashLoader color="#3cb4ac" />
           </div>
         ) : (
           <>
-            <div className="flex flex-col gap-y-5 items-center">
-              <img src={CheckmarkIcon} alt="check logo" />
-              <p className="text-4 font-bold">Thanks for your support!</p>
-              <p className="leading-6 text-5 text-neutral-2">
+            <div className="flex flex-col gap-y-5 items-center tablet:gap-y-3">
+              <img
+                src={CheckmarkIcon}
+                alt="check logo"
+                className="tablet:w-[5.5rem] tablet:mb-9"
+              />
+              <p className="text-4 font-bold tablet:text-2.5">
+                Thanks for your support!
+              </p>
+              <p className="leading-6 text-5 text-neutral-2 tablet:text-4.5 tablet:leading-8">
                 Your pledge brings us one step closer to sharing Mastercraft
                 Bamboo Monitor Riser worldwide. You will get an email once our
                 campaign is completed.
@@ -31,7 +40,7 @@ const PledgeConfirmationModal = ({
             <button
               type="button"
               onClick={closeModal}
-              className="px-8 py-3 rounded-full text-white text-5 font-bold bg-primary-1"
+              className="px-8 py-3 rounded-full text-white text-5 font-bold bg-primary-1 transition-colors hover:bg-teal-400 tablet:py-2 tablet:active:bg-primary-2"
             >
               Got it!
             </button>
