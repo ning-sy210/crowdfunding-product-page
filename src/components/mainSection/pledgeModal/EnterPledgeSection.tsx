14d6f21d4f38663d14d4155aebeeea095dadede8
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-11-22 23:19:43 +0800

    style: add accessibility and active/hover states

--- src/components/mainSection/pledgeModal/EnterPledgeSection.tsx
+++ src/components/mainSection/pledgeModal/EnterPledgeSection.tsx
@@ -37,26 +37,32 @@ const EnterPledgeSection = ({
 
   return (
     <section className="flex flex-col text-center gap-y-5 pt-6 pb-6 px-6 border-t border-slate-300 tablet:flex-row tablet:items-center tablet:justify-between">
-      <p className="text-5 text-neutral-2 leading-6 tablet:text-4.51">
+      <label
+        htmlFor="pledge input"
+        className="text-5 text-neutral-2 leading-6 tablet:text-4.51"
+      >
         Enter your pledge
-      </p>
+      </label>
 
       <div className="relative flex justify-between tablet:order-2">
-        <span className="absolute top-[13px] left-6 text-5 font-bold text-stone-400 tablet:top-2">
+        <span className="absolute top-[13px] left-6 text-5 font-bold text-stone-400 cursor-pointer tablet:top-2">
           $
         </span>
 
         <input
+          id="pledge input"
           type="number"
           defaultValue={minPledgeAmount.toString()}
           min={minPledgeAmount}
           onChange={(e) => handleInputChange(e.target.value)}
-          className="border border-slate-300 rounded-full w-[43%] max-w-[100px] pl-10 pr-4 text-5 font-bold tablet:max-w-[10rem]"
+          onKeyDown={(e) => e.key === "Enter" && validateInput(pledgeAmount)}
+          className="border border-slate-300 rounded-full w-[43%] max-w-[100px] pl-10 pr-4 text-5 font-bold cursor-pointer outline-none focus:border-primary-1 tablet:max-w-[10rem] tablet:active:border-primary-2"
         ></input>
         <button
           type="button"
           onClick={() => validateInput(pledgeAmount)}
-          className="bg-primary-1 text-white rounded-full w-1/2 max-w-[120px] py-[13px] text-5 font-bold tablet:py-2 tablet:max-w-[10rem]"
+          className="bg-primary-1 text-white rounded-full w-1/2 max-w-[120px] py-[13px] text-5 font-bold transition-colors  
+          tablet:py-2 tablet:max-w-[10rem] tablet:hover:bg-teal-400 tablet:active:bg-primary-2"
         >
           Continue
         </button>
