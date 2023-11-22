7aee7a247dca6f3610d7d6b1f8995bea169f7a21
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-11-22 22:49:55 +0800

    feat: make `<PledgeSection />` responsive

--- src/components/mainSection/pledgeModal/EnterPledgeSection.tsx
+++ src/components/mainSection/pledgeModal/EnterPledgeSection.tsx
@@ -28,7 +28,7 @@ const EnterPledgeSection = ({
 
     const amount = parseInt(pledgeAmount);
     if (amount < minPledgeAmount) {
-      setErrorMessage(`Pledge amount must be at least $${minPledgeAmount}!`);
+      setErrorMessage(`Amount must be at least $${minPledgeAmount}!`);
       return;
     }
 
@@ -36,11 +36,13 @@ const EnterPledgeSection = ({
   }
 
   return (
-    <section className="flex flex-col text-center gap-y-5 pt-6 pb-6 px-6 border-t border-slate-300">
-      <p className="text-5 text-neutral-2 leading-6">Enter your pledge</p>
+    <section className="flex flex-col text-center gap-y-5 pt-6 pb-6 px-6 border-t border-slate-300 tablet:flex-row tablet:items-center tablet:justify-between">
+      <p className="text-5 text-neutral-2 leading-6 tablet:text-4.51">
+        Enter your pledge
+      </p>
 
-      <div className="relative flex justify-between">
-        <span className="absolute top-[13px] left-6 text-5 font-bold text-stone-400">
+      <div className="relative flex justify-between tablet:order-2">
+        <span className="absolute top-[13px] left-6 text-5 font-bold text-stone-400 tablet:top-2">
           $
         </span>
 
@@ -49,19 +51,21 @@ const EnterPledgeSection = ({
           defaultValue={minPledgeAmount.toString()}
           min={minPledgeAmount}
           onChange={(e) => handleInputChange(e.target.value)}
-          className="w-[43%] border border-slate-300 rounded-full pl-10 pr-4 text-5 font-bold"
+          className="border border-slate-300 rounded-full w-[43%] max-w-[100px] pl-10 pr-4 text-5 font-bold tablet:max-w-[10rem]"
         ></input>
         <button
           type="button"
           onClick={() => validateInput(pledgeAmount)}
-          className="bg-primary-1 text-white rounded-full w-1/2 py-[13px] text-5 font-bold"
+          className="bg-primary-1 text-white rounded-full w-1/2 max-w-[120px] py-[13px] text-5 font-bold tablet:py-2 tablet:max-w-[10rem]"
         >
           Continue
         </button>
       </div>
 
       {errorMessage && (
-        <div className="text-5 text-red-400 font-medium">{errorMessage}</div>
+        <div className="text-5 text-red-400 font-medium tablet:order-[1]">
+          {errorMessage}
+        </div>
       )}
     </section>
   );
