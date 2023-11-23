74a6e49702a85c811acdf54820a999e5699b6408
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-11-23 13:48:43 +0800

    feat: make `<PledgeModalOption />` responsive

--- src/components/mainSection/pledgeModal/PledgeModalOption.tsx
+++ src/components/mainSection/pledgeModal/PledgeModalOption.tsx
@@ -28,8 +28,14 @@ const PledgeModalOption = ({
       } [&:has(input:checked)]:border-primary-1 [&:has(input:checked)]:border-2`}
       onClick={outOfStock ? () => null : onOptionSelect}
     >
-      <section className="flex flex-col gap-y-5 px-6 pt-5 pb-7">
-        <div className="flex items-center gap-x-4">
+      <section
+        className="flex flex-col gap-y-5 px-6 pt-5 pb-7
+        tablet:grid tablet:grid-rows-[auto_auto] tablet:grid-cols-[auto_auto_min-content] tablet:gap-x-6 tablet:gap-y-0"
+      >
+        <div
+          className="flex items-center gap-x-4
+          tablet:grid tablet:col-span-2 tablet:grid-cols-[subgrid] tablet:gap-x-[unset]"
+        >
           {/* TODO: Change appearance of radio button */}
           <input
             id={`${reward} input`}
@@ -43,28 +49,37 @@ const PledgeModalOption = ({
             htmlFor={`${reward} input`}
             className="flex flex-col h-12 justify-center"
           >
-            <div className="flex flex-col gap-y-1 justify-between">
-              <span className="text-5 font-bold hover:cursor-pointer tablet:active:text-primary-1">
+            <div className="flex flex-col gap-y-1 justify-between tablet:flex-row tablet:gap-x-4 tablet:justify-normal">
+              <span className="text-5 font-bold hover:cursor-pointer tablet:active:text-primary-1 tablet:text-4.5">
                 {reward}
               </span>
 
               {/* Min pledge amount label */}
               {isPaidOption && (
-                <span className="text-5 font-medium text-primary-1">
+                <span
+                  className={`text-5 font-medium text-primary-1 tablet:text-4.5${
+                    checked ? " tablet:font-bold" : ""
+                  }`}
+                >
                   Pledge ${minPledgeAmount.toLocaleString()} or more
                 </span>
               )}
             </div>
           </label>
         </div>
-        <p className="text-5 text-neutral-2 leading-6">{desc}</p>
+        <p
+          className="text-5 text-neutral-2 leading-6 
+          tablet:row-start-2 tablet:row-span-full tablet:col-start-2 tablet:col-span-full tablet:text-4.51 tablet:leading-7"
+        >
+          {desc}
+        </p>
 
         {/* Stock left label */}
         {isPaidOption && (
           <p
             className={`flex items-center gap-x-2${
               lowInStock ? " text-red-500" : ""
-            }`}
+            } tablet:justify-self-end`}
           >
             <span className="text-4 font-bold">{stock}</span>
             <span className="text-5 text-neutral-2 leading-6">left</span>
